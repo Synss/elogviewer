07b36b57f7fba89865c71f578c03730ee6ee72f9
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2019-10-23 22:44:41 +0200

    setup: Complete `setup.py`

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
-"""
-Usage:
-    python setup.py sdist
-"""
-
-import os
-from distutils.core import setup
-
-import elogviewer
+from setuptools import setup
 
 setup(
     name="elogviewer",
-    version=elogviewer.__version__,
+    version="2.9",
     author="Mathias Laurin",
     author_email="Mathias.Laurin+gentoo.org@gmail.com",
     url="http://sourceforge.net/projects/elogviewer/",
     license="GPLv2",
+    install_requires=["PyQt5"],
+    tests_require=["pytest", "isort", "black"],
+    test_suire="tests",
     data_files=[("", ["elogviewer.1", "LICENSE.TXT"])],
     scripts=["elogviewer.py"],
-    classifiers=os.linesep.join(
-        s
-        for s in """
-          Development Status :: 5 - Production/Stable
-          License :: OSI Approved :: GNU General Public License v2 (GPLv2)
-          Programming Language :: Python :: 3
-          Environment :: X11 Applications :: Qt
-          """
+    classifiers="\n".join(
+        (
+            "Development Status :: 5 - Production/Stable",
+            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+            "Programming Language :: Python :: 3",
+            "Environment :: X11 Applications :: Qt",
+        )
     ),
 )
